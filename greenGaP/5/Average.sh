7502c508d122020e42d1eee989293fbfd10bcb51
Daniel Tanner <danielsptanner@gmail.com>
2025-03-12 21:05:05 +0100

    It's actually the average supercell height that it averages. Comment added

--- greenGaP/5/Average.sh
+++ greenGaP/5/Average.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# This cript returns the average size in the C lattice vector direction of all the configs.
+
 for i in Config*; do  tail $i/log.lammps | grep 'c= [0-9][0-9]*'; done | awk '{s += $2} END {printf "%10.5f\n" ,s/NR}'
